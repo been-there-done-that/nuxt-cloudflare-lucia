a9b44fb9a58f8fe62d0e1ba880e5cf1f8839aee1
jagadeeswara_reddy <jagadeeswara.reddy@otocapital.in>
2023-08-10 16:53:23 +0530

    d1 thingy :/

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -2,13 +2,32 @@ import lucia from "lucia-auth";
 import { d1 } from "@lucia-auth/adapter-sqlite";
 import { h3 } from "lucia-auth/middleware";
 import type { Auth as LAuth, Configuration } from "lucia-auth";
+import { betterSqlite3 } from "@lucia-auth/adapter-sqlite";
+import sqlite from "better-sqlite3";
 
 let _auth: LAuth<Configuration> | null = null;
 
+const mapping = {
+  user: "user",
+  key: "user_key",
+  session: "user_session",
+};
+
 export const useAuth = () => {
   if (!_auth) {
+    // we will do this calculations internally and decide which database to pick.
+
+    let _adapter = null;
+
+    if (process.dev) {
+      const db = sqlite("main.db");
+      _adapter = betterSqlite3(db, mapping);
+    } else {
+      _adapter = d1(process.env.DB);
+    }
+
     _auth = lucia({
-      adapter: d1(process.env.DB),
+      adapter: _adapter,
       env: process.dev ? "DEV" : "PROD",
       middleware: h3(),
       transformDatabaseUser: (userData) => {
